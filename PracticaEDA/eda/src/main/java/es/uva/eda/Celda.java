de6e1b23aadc5f6063343f38be15caf10a122afb
cardstdani <danielgarciasolla0@gmail.com>
2023-09-25 17:49:39 +0200

    Created Maven Project

---
+++ PracticaEDA/eda/src/main/java/es/uva/eda/Celda.java
@@ -0,0 +1,7 @@
+package es.uva.eda;
+
+public interface Celda {
+	public void Inicializar(int n);
+	public void RayoCosmico(int i, int j);
+	public boolean Cortocircuito();
+}
---
+++ PracticaEDA/eda/src/main/java/es/uva/eda/CeldaSimple.java
@@ -0,0 +1,23 @@
+package es.uva.eda;
+
+public class CeldaSimple implements Celda{
+
+	@Override
+	public void Inicializar(int n) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void RayoCosmico(int i, int j) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean Cortocircuito() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
