58a024ca07b53a9d09f14a26613964a19b25a64e
cardstdani <danielgarciasolla0@gmail.com>
2023-10-07 19:04:18 +0200

    Funcion helper modificada

--- PracticaEDA/eda/src/main/java/es/uva/eda/CeldaSimple.java
+++ PracticaEDA/eda/src/main/java/es/uva/eda/CeldaSimple.java
@@ -6,6 +6,7 @@ import com.aparapi.*;
 public class CeldaSimple implements Celda {
 	private boolean[][] grid;
 	private boolean[][] visited;
+	private boolean cortocircuito;
 	private int iterations = 0;
 
 	@Override
@@ -24,25 +25,40 @@ public class CeldaSimple implements Celda {
 	public void RayoCosmico(int i, int j) {
 		if (!grid[i][j]) {
 			grid[i][j] = true;
+
+			for (int a = 0; a < grid.length; a++) {
+				for (int b = 0; b < grid[0].length; b++) {
+					visited[a][b] = false;
+				}
+			}
+			cortocircuito = helper(i, j, 0) & helper(i, j, 1);
 		}
 		iterations++;
 	}
 
-	private boolean helper(int i, int j) {
-		if (i == (grid.length - 1)) {
-			return true;
-		}
+	private boolean helper(int i, int j, int up) {
 		visited[i][j] = true;
-		int[][] nei = new int[][] { { Math.max(0, i - 1), j }, { Math.min(grid.length - 1, i + 1), j },
-				{ i, Math.max(0, j - 1) }, { i, Math.min(grid[0].length - 1, j + 1) },
-				{ Math.max(0, i - 1), Math.max(0, j - 1) },
-				{ Math.min(grid.length - 1, i + 1), Math.min(grid[0].length - 1, j + 1) },
-				{ Math.max(0, i - 1), Math.min(grid[0].length - 1, j + 1) },
-				{ Math.min(grid.length - 1, i + 1), Math.max(0, j - 1) } };
+		int[][] nei;
+		if (up == 0) {
+			if (i == (grid.length - 1)) {
+				return true;
+			}
+			nei = new int[][] { { i, Math.max(0, j - 1) }, { i, Math.min(grid[0].length - 1, j + 1) },
+					{ Math.min(grid.length - 1, i + 1), j },
+					{ Math.min(grid.length - 1, i + 1), Math.min(grid[0].length - 1, j + 1) },
+					{ Math.min(grid.length - 1, i + 1), Math.max(0, j - 1) } };
+		} else {
+			if (i == 0) {
+				return true;
+			}
+			nei = new int[][] { { i, Math.max(0, j - 1) }, { i, Math.min(grid[0].length - 1, j + 1) },
+					{ Math.max(0, i - 1), j }, { Math.max(0, i - 1), Math.min(grid[0].length - 1, j + 1) },
+					{ Math.max(0, i - 1), Math.max(0, j - 1) } };
+		}
 
 		for (int[] k : nei) {
 			if (!visited[k[0]][k[1]] & grid[k[0]][k[1]]) {
-				if (helper(k[0], k[1])) {
+				if (helper(k[0], k[1], up)) {
 					return true;
 				}
 			}
@@ -52,20 +68,7 @@ public class CeldaSimple implements Celda {
 
 	@Override
 	public boolean Cortocircuito() {
-		for (int i = 0; i < grid.length; i++) {
-			for (int j = 0; j < grid[0].length; j++) {
-				visited[i][j] = false;
-			}
-		}
-
-		for (int i = 0; i < grid[0].length; i++) {
-			if (grid[0][i]) {
-				if (helper(0, i)) {
-					return true;
-				}
-			}
-		}
-		return false;
+		return cortocircuito;
 	}
 
 	@Override
