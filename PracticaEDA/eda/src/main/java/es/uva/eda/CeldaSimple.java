76611fa07380af3859f65f0335b3291356c24e59
Daniel García Solla <danielgarciasolla0@gmail.com>
2023-10-17 12:40:27 +0200

    Optimizado???

--- PracticaEDA/eda/src/main/java/es/uva/eda/CeldaSimple.java
+++ PracticaEDA/eda/src/main/java/es/uva/eda/CeldaSimple.java
@@ -3,6 +3,8 @@ package es.uva.eda;
 public class CeldaSimple implements Celda {
 	private boolean[][] grid;
 	private boolean[][] visited;
+	private int[][] neiUp = new int[][] { { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, -1 }, { 0, 1 } };
+	private int[][] neiDown = new int[][] { { 1, -1 }, { 1, 0 }, { 1, 1 }, { 0, -1 }, { 0, 1 } };
 	private boolean cortocircuito;
 
 	@Override
@@ -20,43 +22,50 @@ public class CeldaSimple implements Celda {
 
 	@Override
 	public void RayoCosmico(int i, int j) {
-		//f(x)=n^2 (1-(1 - 1/n^2)^x) Numero estimado de atomos cambiados en iteracion x
-		if (!grid[i][j]) { //P(x)=(1-(1/n^2))^x probabilidad de entrar en esta condición = n^2-f(x) / n^2
+		// B(x)=n^2 (1-(1 - 1/n^2)^x) Numero estimado de atomos cambiados en iteracion x
+		if (!grid[i][j]) { // P(x)=(1-(1/n^2))^x probabilidad de entrar en esta condición = n^2-f(x) / n^2
 			grid[i][j] = true;
 
-			for (int a = 0; a < grid.length; a++) { //O(n^2)
+			for (int a = 0; a < grid.length; a++) { // O(n^2)
 				for (int b = 0; b < grid[0].length; b++) {
 					visited[a][b] = false;
 				}
 			}
-			cortocircuito = helper(i, j, 0) & helper(i, j, 1); //O(n^2) (peor caso)
-		}		
+			cortocircuito = helper(i, j, 0);
+			for (int a = 0; a < grid.length; a++) { // O(n^2)
+				for (int b = 0; b < grid[0].length; b++) {
+					visited[a][b] = false;
+				}
+			}
+			cortocircuito = cortocircuito && helper(i, j, 1);
+		}
 	}
 
-	//Complejidad T(i, j, x)=(1-P(x)) (T(i, j-1)+T(i, j+1)+T(i-1, j)+T(i-1, j-1)+T(i-1, j+1)) + O(1)
+	// Complejidad T(i, j, x)=(1-P(x)) (T(i, j-1)+T(i, j+1)+T(i-1, j)+T(i-1,
+	// j-1)+T(i-1, j+1)) + O(1)
 	private boolean helper(int i, int j, int up) {
+		if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || !grid[i][j] || visited[i][j]) {
+			return false;
+		}
 		visited[i][j] = true;
-		int[][] nei;
 		if (up == 0) {
 			if (i == (grid.length - 1)) {
 				return true;
 			}
-			nei = new int[][] { { i, Math.max(0, j - 1) }, { i, Math.min(grid[0].length - 1, j + 1) },
-					{ Math.min(grid.length - 1, i + 1), j },
-					{ Math.min(grid.length - 1, i + 1), Math.min(grid[0].length - 1, j + 1) },
-					{ Math.min(grid.length - 1, i + 1), Math.max(0, j - 1) } };
+
+			for (int k = 0; k < neiDown.length; k++) {
+				if (helper(neiDown[k][0] + i, neiDown[k][1] + j, up)) {
+					return true;
+				}
+			}
+
 		} else {
 			if (i == 0) {
 				return true;
 			}
-			nei = new int[][] { { i, Math.max(0, j - 1) }, { i, Math.min(grid[0].length - 1, j + 1) },
-					{ Math.max(0, i - 1), j }, { Math.max(0, i - 1), Math.min(grid[0].length - 1, j + 1) },
-					{ Math.max(0, i - 1), Math.max(0, j - 1) } };
-		}
 
-		for (int[] k : nei) {
-			if (!visited[k[0]][k[1]] & grid[k[0]][k[1]]) {
-				if (helper(k[0], k[1], up)) {
+			for (int k = 0; k < neiUp.length; k++) {
+				if (helper(neiUp[k][0] + i, neiUp[k][1] + j, up)) {
 					return true;
 				}
 			}
