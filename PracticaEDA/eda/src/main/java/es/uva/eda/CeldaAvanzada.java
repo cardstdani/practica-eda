b9a34b1e8df01d7f3f82cde2f3f4ee66345b0d3f
cardstdani <danielgarciasolla0@gmail.com>
2023-11-15 20:21:32 +0100

    🤗🤗

--- PracticaEDA/eda/src/main/java/es/uva/eda/CeldaAvanzada.java
+++ PracticaEDA/eda/src/main/java/es/uva/eda/CeldaAvanzada.java
@@ -1,23 +1,28 @@
 package es.uva.eda;
 
-class DisjointSet {
+public class CeldaAvanzada implements Celda {
+	private boolean[][] grid;
+	private int[][] nei = new int[][] { { -1, 0 }, { 1, 0 }, { 1, -1 }, { 1, 1 }, { -1, -1 }, { -1, 1 }, { 0, -1 },
+			{ 0, 1 } };
+	private boolean cortocircuito;
+	private int n;
+
+	// Disjoint Set
 	private int[] parent;
 	private int[] rank;
 
-	public DisjointSet(int size) {
-		parent = new int[size];
-		rank = new int[size];
-		for (int i = 0; i < size; i++) {
-			parent[i] = i;
-			rank[i] = 0;
-		}
-	}
-
 	public int find(int item) {
-		if (parent[item] != item) {
-			parent[item] = find(parent[item]);
+		int root = item;
+		while (root != parent[root]) {
+			root = parent[root];
 		}
-		return parent[item];
+		// Compress the path
+		while (item != root) {
+			int next = parent[item];
+			parent[item] = root;
+			item = next;
+		}
+		return root;
 	}
 
 	public void union(int item1, int item2) {
@@ -34,15 +39,6 @@ class DisjointSet {
 			}
 		}
 	}
-}
-
-public class CeldaAvanzada implements Celda {
-	private boolean[][] grid;
-	private int[][] nei = new int[][] { { -1, 0 }, { 1, 0 }, { 1, -1 }, { 1, 1 }, { -1, -1 }, { -1, 1 }, { 0, -1 },
-			{ 0, 1 } };
-	private boolean cortocircuito;
-	private DisjointSet ds;
-	private int n;
 
 	/**
 	 * Inicializa la cuadrícula con un tamaño específico.
@@ -54,17 +50,19 @@ public class CeldaAvanzada implements Celda {
 		this.n = n;
 		grid = new boolean[n][n];
 		cortocircuito = false;
-		ds = new DisjointSet((n * n) + 2);
-
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				grid[i][j] = false;
 			}
 		}
-	}
 
-	private int getIndex(int i, int j) {
-		return i * n + j;
+		int size = (n * n) + 2;
+		parent = new int[size];
+		rank = new int[size];
+		for (int i = 0; i < size; i++) {
+			parent[i] = i;
+			rank[i] = 0;
+		}
 	}
 
 	/**
@@ -79,23 +77,23 @@ public class CeldaAvanzada implements Celda {
 	public void RayoCosmico(int i, int j) {
 		if (!grid[i][j]) {
 			grid[i][j] = true;
-			int cellIndex = getIndex(i, j);
+			int cellIndex = i * n + j;
 
 			if (i == 0) {
-				ds.union(cellIndex, n * n); // Connect to virtual top node
+				union(cellIndex, n * n); // Connect to virtual top node
 			} else if (i == n - 1) {
-				ds.union(cellIndex, n * n + 1); // Connect to virtual bottom node
+				union(cellIndex, n * n + 1); // Connect to virtual bottom node
 			}
 
 			for (int[] k : nei) {
 				int newI = i + k[0];
 				int newJ = j + k[1];
 				if (newI >= 0 && newJ >= 0 && newI < n && newJ < n && grid[newI][newJ]) {
-					ds.union(cellIndex, getIndex(newI, newJ));
+					union(cellIndex, newI * n + newJ);
 				}
 			}
 
-			cortocircuito = ds.find(n * n) == ds.find(n * n + 1);
+			cortocircuito = find(n * n) == find(n * n + 1);
 		}
 	}
 
